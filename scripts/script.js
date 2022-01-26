d6146fe8d790b0d2acf911567516e860bdf06ff9
Danny <36959138+DannyStoyanov@users.noreply.github.com>
2022-01-26 19:23:06 +0200

    add minor navigation design

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,24 @@ const friendListButtonElement=document.getElementById('open-friend-list-btn');
 // Default onload page state
 window.addEventListener('load', (event) => {
     friendListElement.classList.add('hidden');
+    friendListButtonElement.classList.remove('active-menu-option');
+    chatListButtonElement.classList.add('active-menu-option');
+
     // chatListElement.classList.add("hidden");
 });
 
 chatListButtonElement.addEventListener('click', (event) => {
+    chatListButtonElement.classList.add('active-menu-option');
+    friendListButtonElement.classList.remove('active-menu-option');
+
     chatListElement.classList.remove('hidden');
     friendListElement.classList.add('hidden');
 });
 
 friendListButtonElement.addEventListener('click', (event) => {
+    friendListButtonElement.classList.add('active-menu-option');
+    chatListButtonElement.classList.remove('active-menu-option');
+
     friendListElement.classList.remove('hidden');
     chatListElement.classList.add('hidden');
 });
