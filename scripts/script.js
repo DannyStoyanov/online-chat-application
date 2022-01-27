58794ce451e4ca7a1db4efa9ae68155f57f11767
Danny <36959138+DannyStoyanov@users.noreply.github.com>
2022-01-27 18:46:08 +0200

    add overall design tweaks

--- scripts/script.js
+++ scripts/script.js
@@ -3,12 +3,18 @@ const chatListElement=document.getElementById('chat-list-wrapper');
 const chatListButtonElement=document.getElementById('open-chat-list-btn');
 const friendListButtonElement=document.getElementById('open-friend-list-btn');
 const logOutButtonElement=document.getElementById('log-out-btn');
+const filterAllButtonElement=document.getElementById('filter-all-btn');
+const filterUnreadButtonElement=document.getElementById('filter-unread-btn');
+const filterGroupsButtonElement=document.getElementById('filter-groups-btn');
 
 // Default onload page state
 window.addEventListener('load', (event) => {
     friendListElement.classList.add('hidden');
     friendListButtonElement.classList.remove('active-menu-option');
     chatListButtonElement.classList.add('active-menu-option');
+
+    // Chat default filter
+    filterAllButtonElement.classList.add('current-filter');
 
     // chatListElement.classList.add("hidden");
     updateTitle();
@@ -52,4 +58,27 @@ logOutButtonElement.addEventListener('click', (event) => {
 // Redirect to login page
 function logOut() {
     window.location.href="index.html";
-  }
+}
+
+// Chat settings:
+
+// Filter all messages
+filterAllButtonElement.addEventListener('click',(event) => {
+    filterAllButtonElement.classList.add('current-filter');
+    filterUnreadButtonElement.classList.remove('current-filter');
+    filterGroupsButtonElement.classList.remove('current-filter');
+});
+
+// Filter unread messages
+filterUnreadButtonElement.addEventListener('click',(event) => {
+    filterUnreadButtonElement.classList.add('current-filter');
+    filterAllButtonElement.classList.remove('current-filter');
+    filterGroupsButtonElement.classList.remove('current-filter');
+});
+
+// Filter groups messages
+filterGroupsButtonElement.addEventListener('click',(event) => {
+    filterGroupsButtonElement.classList.add('current-filter');
+    filterAllButtonElement.classList.remove('current-filter');
+    filterUnreadButtonElement.classList.remove('current-filter');
+});
